f3ebd9af6b5c3bb6ca1e22b19e6deba2b254cf32
mousavi007 <mousavi_1986@yahoo.com>
2017-02-13 19:02:21 +0330

    ایجاد اینترفیس برای get

--- app/src/main/java/com/example/mousavi/git007/View/MainActivity.java
+++ app/src/main/java/com/example/mousavi/git007/View/MainActivity.java
@@ -3,10 +3,16 @@ package com.example.mousavi.git007.View;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import com.example.mousavi.git007.ViewModel.Github;
 import com.example.mousavi.git007.pojo.User;
 import com.example.mousavi.git007.model.user;
 import com.example.mousavi.git007.R;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView t1=(TextView)findViewById(R.id.t1);
-
+        String url="";
+        Github github=new Retrofit.Builder().baseUrl(url).addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build().create(Github.class);;
     }
 }
