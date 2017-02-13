982c7d991e928c6bf413055a1072f38e676dfb54
mousavi007 <mousavi_1986@yahoo.com>
2017-02-13 12:55:40 +0330

    Initial commit

---
+++ app/src/main/java/com/example/mousavi/git007/View/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.mousavi.git007.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mousavi.git007.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
