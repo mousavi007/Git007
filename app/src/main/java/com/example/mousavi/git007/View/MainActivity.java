46656f4ef9c2a720f449938bf4d4550356731065
mousavi007 <mousavi_1986@yahoo.com>
2017-03-04 12:54:47 +0330

    add retrofit manager

--- app/src/main/java/com/example/mousavi/git007/View/MainActivity.java
+++ app/src/main/java/com/example/mousavi/git007/View/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.mousavi.git007.View;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -14,6 +17,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import com.example.mousavi.git007.managers.RetrofitManager;
 import com.example.mousavi.git007.models.pojo.AuthEntity;
 import com.example.mousavi.git007.models.pojo.NetConstant;
+import com.example.mousavi.git007.models.pojo.TokenStorer;
 import com.example.mousavi.git007.network.Github;
 import com.example.mousavi.git007.models.pojo.User;
 import com.example.mousavi.git007.R;
@@ -24,26 +28,55 @@ import io.reactivex.schedulers.Schedulers;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 public class MainActivity extends AppCompatActivity {
 
+    public static String OAUTH_URL = "https://github.com/login/oauth/authorize";
+    public static String OAUTH_ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token";
+
+    public static String CLIENT_ID = "5bf29364071543db722d";
+    public static String CLIENT_SECRET = "92201c29b73d7a183286f6b7ad0c0b08760b1c02";
+    public static String CALLBACK_URL = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText e1=(EditText)findViewById(R.id.editText);
-        EditText e2=(EditText)findViewById(R.id.editText2);
-        TextView t3=(TextView)findViewById(R.id.t3);
-        Button b1=(Button)findViewById(R.id.button);
-        String user= e1.getText().toString();
-        String pass= e2.getText().toString();
-       b1.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               Observable<AuthEntity> observable= RetrofitManager.newInstance().getService()
-                       .login(NetConstant.MD_APP_KEY,NetConstant.MD_APP_SECRET, LoginService.GrantType.PASSWORD,user,pass);
-               observable.subscribeOn(Schedulers.io())
-                       .observeOn(AndroidSchedulers.mainThread()).subscribe((x)->{t3.setText(x.getExpires_in());});
-           }
-       });
+        String url = OAUTH_URL + "?client_id=" + CLIENT_ID;
 
+        WebView webview = (WebView)findViewById(R.id.webview01);
+        webview.getSettings().setJavaScriptEnabled(true);
+        final TextView t=(TextView)findViewById(R.id.textView);
+
+        webview.setWebViewClient(new WebViewClient() {
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                String accessTokenFragment = "access_token=";
+                String accessCodeFragment = "code=";
+                TokenStorer t1=new TokenStorer();
+                // We hijack the GET request to extract the OAuth parameters
+
+                if (url.contains(accessTokenFragment)) {
+                    // the GET request contains directly the token
+                    String accessToken = url.substring(url.indexOf(accessTokenFragment));
+                    t1.setAccessToken(accessToken);
+                    t.setText(t1.getAccessToken());
+
+                } else if(url.contains(accessCodeFragment)) {
+                    // the GET request contains an authorization code
+                    String accessCode = url.substring(url.indexOf(accessCodeFragment)+5);
+                    t1.setAccessCode(accessCode);
+
+
+                    String query = "client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&code=" + accessCode;
+                    view.postUrl(OAUTH_ACCESS_TOKEN_URL, query.getBytes());
+                    t.setText(t1.getAccessToken());
+                }
+
+            }
+
+
+
+        });
+        webview.loadUrl(url);
+
+int y =90;
 
     }
 }
---
+++ app/src/main/java/com/example/mousavi/git007/models/pojo/TokenStorer.java
@@ -0,0 +1,28 @@
+package com.example.mousavi.git007.models.pojo;
+
+/**
+ * Created by mousavi on 2017/03/04.
+ */
+
+public class TokenStorer {
+
+    private String accessToken;
+    private String accessCode;
+
+    public String getAccessCode() {
+        return accessCode;
+    }
+
+    public void setAccessCode(String accessCode) {
+        this.accessCode = accessCode;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+}
