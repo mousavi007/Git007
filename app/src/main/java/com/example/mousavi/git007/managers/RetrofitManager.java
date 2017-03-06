0bfb0e1ae311dc9976aee8dac0b3c9a1ba187167
darya <mousavi_1986@yahoo.com>
2017-03-06 15:37:42 +0330

    no change

--- app/src/main/java/com/example/mousavi/git007/managers/RetrofitManager.java
+++ app/src/main/java/com/example/mousavi/git007/managers/RetrofitManager.java
@@ -3,6 +3,8 @@ package com.example.mousavi.git007.managers;
 import com.example.mousavi.git007.network.Github;
 import com.example.mousavi.git007.network.LoginService;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -16,8 +18,14 @@ public class RetrofitManager {
      * Constructor used to build MovieDB service.
      */
     private RetrofitManager() {
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor).build();
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(okHttpClient)
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
