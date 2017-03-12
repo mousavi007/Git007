6e40c0e968d1aead514bcdda42eadcc3dee09d06
darya <mousavi_1986@yahoo.com>
2017-03-12 08:02:00 +0330

    no change

--- app/src/main/java/com/example/mousavi/git007/models/pojo/User.java
+++ app/src/main/java/com/example/mousavi/git007/models/pojo/User.java
@@ -10,112 +10,112 @@ public class User {
 
     @SerializedName("login")
     @Expose
-    private String login;
+    private static String login;
     @SerializedName("id")
     @Expose
-    private Integer id;
+    private static Integer id;
     @SerializedName("avatar_url")
     @Expose
-    private String avatarUrl;
+    private static String avatarUrl;
     @SerializedName("gravatar_id")
     @Expose
-    private String gravatarId;
+    private static String gravatarId;
     @SerializedName("url")
     @Expose
-    private String url;
+    private static String url;
     @SerializedName("html_url")
     @Expose
-    private String htmlUrl;
+    private static String htmlUrl;
     @SerializedName("followers_url")
     @Expose
-    private String followersUrl;
+    private static String followersUrl;
     @SerializedName("following_url")
     @Expose
-    private String followingUrl;
+    private static String followingUrl;
     @SerializedName("gists_url")
     @Expose
-    private String gistsUrl;
+    private static String gistsUrl;
     @SerializedName("starred_url")
     @Expose
-    private String starredUrl;
+    private static String starredUrl;
     @SerializedName("subscriptions_url")
     @Expose
-    private String subscriptionsUrl;
+    private static String subscriptionsUrl;
     @SerializedName("organizations_url")
     @Expose
-    private String organizationsUrl;
+    private static String organizationsUrl;
     @SerializedName("repos_url")
     @Expose
-    private String reposUrl;
+    private static String reposUrl;
     @SerializedName("events_url")
     @Expose
-    private String eventsUrl;
+    private static String eventsUrl;
     @SerializedName("received_events_url")
     @Expose
-    private String receivedEventsUrl;
+    private static String receivedEventsUrl;
     @SerializedName("type")
     @Expose
-    private String type;
+    private static String type;
     @SerializedName("site_admin")
     @Expose
-    private Boolean siteAdmin;
+    private static Boolean siteAdmin;
     @SerializedName("name")
     @Expose
-    private Object name;
+    private static Object name;
     @SerializedName("company")
     @Expose
-    private Object company;
+    private static Object company;
     @SerializedName("blog")
     @Expose
-    private Object blog;
+    private static Object blog;
     @SerializedName("location")
     @Expose
-    private Object location;
+    private static Object location;
     @SerializedName("email")
     @Expose
-    private Object email;
+    private static Object email;
     @SerializedName("hireable")
     @Expose
-    private Object hireable;
+    private static Object hireable;
     @SerializedName("bio")
     @Expose
-    private Object bio;
+    private static Object bio;
     @SerializedName("public_repos")
     @Expose
-    private Integer publicRepos;
+    private static Integer publicRepos;
     @SerializedName("public_gists")
     @Expose
-    private Integer publicGists;
+    private static Integer publicGists;
     @SerializedName("followers")
     @Expose
-    private Integer followers;
+    private static Integer followers;
     @SerializedName("following")
     @Expose
-    private Integer following;
+    private static Integer following;
     @SerializedName("created_at")
     @Expose
-    private String createdAt;
+    private static String createdAt;
     @SerializedName("updated_at")
     @Expose
-    private String updatedAt;
+    private static String updatedAt;
     @SerializedName("private_gists")
     @Expose
-    private Integer privateGists;
+    private static Integer privateGists;
     @SerializedName("total_private_repos")
     @Expose
-    private Integer totalPrivateRepos;
+    private static Integer totalPrivateRepos;
     @SerializedName("owned_private_repos")
     @Expose
-    private Integer ownedPrivateRepos;
+    private static Integer ownedPrivateRepos;
     @SerializedName("disk_usage")
     @Expose
-    private Integer diskUsage;
+    private static Integer diskUsage;
     @SerializedName("collaborators")
     @Expose
-    private Integer collaborators;
+    private static Integer collaborators;
     @SerializedName("two_factor_authentication")
     @Expose
-    private Boolean twoFactorAuthentication;
+    private static Boolean twoFactorAuthentication;
     @SerializedName("plan")
     @Expose
     private Plan plan;
